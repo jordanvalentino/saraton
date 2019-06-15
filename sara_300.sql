370151659bdd50a434447ec9b0ef22a8c1501421
Nadares <andreasivan181@gmail.com>
2019-06-15 13:26:39 +0700

    change sql

--- sara_300.sql
+++ sara_300.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 15, 2019 at 08:21 AM
+-- Generation Time: Jun 15, 2019 at 08:26 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
